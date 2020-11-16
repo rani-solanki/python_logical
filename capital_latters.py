3037a11b8565ee72392fb8b227361bf760f9c8c1
Ranisolanki <59246445+rani-solanki@users.noreply.github.com>
2020-11-16 11:44:04 +0530

    Add files via upload

---
+++ capital_latters.py
@@ -0,0 +1,14 @@
+user=input("enter the string:>")
+index_number = 0
+count =0
+cou=0
+while(index_number<len(user)):
+    if user[index_number].islower():
+        count=count+1
+    elif user[index_number].isupper():
+        cou=cou+1
+    index_number=index_number+1
+print("number of lower case",count)
+print("number of upper case",cou)
+
+
